5a04361a24f4bb6c111fcbabd1c897972e924c11
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2023-05-05 11:29:31 +0530

    Update Var.tf

--- scenarios/cicd/terraform/Var.tf
+++ scenarios/cicd/terraform/Var.tf
@@ -1,4 +1,14 @@
-variable "password" {
-  type = string
-  default = "123password123"
+# variable "password" {
+#   type = string
+#   default = "123password123"
+# }
+
+variable "admin_username" {
+    type = string
+    default = "adminuser"
+}
+
+variable "admin_password" {
+    type = string
+    default = "OurP@**w0rd1998#"
 }
