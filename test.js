8497a3455ea9950d0d0002853e6bc21dfa856ad7
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2023-05-06 00:02:09 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
-const mysql = require('mysql');
+const API_KEY = 'abc123';
 
-const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'S@ltPepp3r&',
-database: 'mydb'
-});
-
-connection.connect(function(err) {
-if (err) throw err;
-console.log('Connected to the database!');
-});
-
-// rest of the code
+function makeRequest() {
+  // Make an API request using the API_KEY
+  // ...
+}
