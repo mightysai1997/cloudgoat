6c52e708e2e35e8bb7721621fdbf14c21a604ee4
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2023-05-05 14:53:03 +0530

    Update index.js

--- scenarios/rce_web_app/assets/rce_app/index.js
+++ scenarios/rce_web_app/assets/rce_app/index.js
@@ -1,1 +1,11 @@
-const password = "mypassword123";
+// const password = "mypassword123";
+const pg = require("pg");
+
+const client = new pg.Client({
+  user: "bob",
+  host: "database.server.com",
+  database: "mydb",
+  password: "correct-horse-battery-staple",
+  port: 3211
+});
+client.connect();
